107dc85b82ec38d8a43b39c433b5f1e1423008f3
Kakon Ray <thisiskakonray@gmail.com>
2024-07-12 00:43:28 +0600

    Dashboard setup complted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 /* import logo from './logo.svg'; */
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Dashboard from './page/Dashboard/Dashboard';
 import AddProduct from './page/AddProduct/AddProduct';
+import Dashboard from './page/Dashboard';
+import DashboardHome from './page/DashboardHome/DashboardHome';
+import ManageProduct from './page/ManageProduct/ManageProduct';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={< Dashboard />} />
-        <Route path='/add/product' element={< AddProduct />} />
+        <Route path="/" element={<Dashboard />}>
+        <Route index element={<DashboardHome />} />
+          <Route path='/manage-product' element={< ManageProduct />} />
+          <Route path='/add-product' element={< AddProduct />} />
+        </Route>
       </Routes>
 
     </>
---
+++ src/page/ManageProduct/ManageProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageProduct = () => {
+    return (
+        <div>
+            <p>This is Product Management</p>
+        </div>
+    );
+};
+
+export default ManageProduct;
