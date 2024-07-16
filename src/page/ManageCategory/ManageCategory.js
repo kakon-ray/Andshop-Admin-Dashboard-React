a17f394a0eaf21db1e30a3ebe1c52cf02b47f8ce
Envobyte Jr Dev <kakon-ray>
2024-07-16 11:07:53 +0600

    Done

--- src/page/ManageCategory/ManageCategory.js
+++ src/page/ManageCategory/ManageCategory.js
@@ -77,6 +77,7 @@ const ManageCategory = () => {
         setShowUpdate(false)
     }
 
+    // delete category
     const handleDelete = (id) => {
         dispatch(deleteCategory(id))
         setShowUpdate(false)
